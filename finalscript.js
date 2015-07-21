09960b92e47f6748e0082b592c6acbf45c7a185e
christychan <christyychan@gmail.com>
2015-07-21 11:52:29 -0700

    Resource
    Added function

--- finalscript.js
+++ finalscript.js
@@ -1,3 +1,129 @@
 var displayArt = function(){
 	document.getElementById("artinfo").style.display = "inline";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayBus = function(){
+	document.getElementById("businfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayCompSci = function(){
+	document.getElementById("compsciinfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayEdu = function(){
+	document.getElementById("eduinfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayEng = function(){
+	document.getElementById("enginfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayEnv = function(){
+	document.getElementById("envinfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayLaw = function(){
+	document.getElementById("lawinfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayMedia = function(){
+	document.getElementById("mediainfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayMed = function(){
+	document.getElementById("medinfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("povinfo").style.display = "none";
+}
+
+var displayPov = function(){
+	document.getElementById("povinfo").style.display = "inline";
+	document.getElementById("artinfo").style.display = "none";
+	document.getElementById("businfo").style.display = "none";
+	document.getElementById("compsciinfo").style.display = "none";
+	document.getElementById("eduinfo").style.display = "none";
+	document.getElementById("enginfo").style.display = "none";
+	document.getElementById("envinfo").style.display = "none";
+	document.getElementById("lawinfo").style.display = "none";
+	document.getElementById("mediainfo").style.display = "none";
+	document.getElementById("medinfo").style.display = "none";
 }
